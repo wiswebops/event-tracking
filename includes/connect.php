5b2c17dc13c1cb959ec742766292b09990a3f122
kststudio <kststudio@gmail.com>
2014-09-15 13:07:26 -0400

    connect updated

--- includes/connect.php
+++ includes/connect.php
@@ -1,4 +1,18 @@
 <?php
+    $url=parse_url(getenv("us-cdbr-iron-east-01.clear.net"));
+
+    $server = $url["54.227.215.252"];
+    $username = $url["b2b2bd32573705"];
+    $password = $url["63a5494f"];
+    $db = substr($url["wishelpd_maps"],1);
+
+    mysqli_connect($server, $username, $password);
+
+
+    mysqli_select_db($db);
+
+/*
+
 define('DB_HOST','54.227.215.252');
 define('DB_USER','b2b2bd32573705');
 define('DB_PASS','63a5494f');
@@ -6,6 +20,6 @@ define('DB_NAME','wishelpd_maps');
 
 $connection = mysql_connect(DB_HOST,DB_USER,DB_PASS) or die(mysql_error());
 mysql_select_db(DB_NAME) or die(mysql_error());
-
+*/
 
 ?>
