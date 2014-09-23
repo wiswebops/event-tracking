b62eb7fbc7b270aad05a926e2e5b2dda9dfa4a49
YIGUO-WIS <yi.guo@wispubs.com>
2014-09-23 16:06:15 -0400

    Update WizardSteps.php

--- WizardSteps.php
+++ WizardSteps.php
@@ -7,10 +7,10 @@ function Event_Create (){
     //return array("status"=>"Success", "EventID"=>$eventData["EventGroupID"]);
 
     //$check_exist1 = mysql_query("SELECT * FROM FloorLevel WHERE iEventID = ".$eventData["EventGroupID"]);
-    $check_exist2 = mysql_query("SELECT * FROM confs WHERE iEventGroupID = ".$eventData["EventGroupID"]);
-    if(mysql_num_rows($check_exist2) == 0)
+    $check_exist2 = mysqli_query($connection,"SELECT * FROM confs WHERE iEventGroupID = ".$eventData["EventGroupID"]);
+    if(mysqli_num_rows($check_exist2) == 0)
     {
-        mysql_query("Insert into confs (confID, name, location, date, dCreated, dLastUpdate, ieventGroupID) values ('".$eventData["confID"]."','".$eventData["EventName"]."','".$eventData["EventLocation"]."','".$eventData["EventDate"]."', NOW(), NOW(),".$eventData["EventGroupID"].");");
+        mysqli_query($connection,"Insert into confs (confID, name, location, date, dCreated, dLastUpdate, ieventGroupID) values ('".$eventData["confID"]."','".$eventData["EventName"]."','".$eventData["EventLocation"]."','".$eventData["EventDate"]."', NOW(), NOW(),".$eventData["EventGroupID"].");");
 
     }
     else
@@ -26,20 +26,20 @@ function Content_Upload (){
    
       //return array("status"=>"Success", "EventID"=>$eventData["ConferenceID"]); 
     
-	 $eventQuery = mysql_query("SELECT * FROM confs WHERE iEventGroupID = ".$eventData["ConferenceID"]);
+	 $eventQuery = mysqli_query($connection,"SELECT * FROM confs WHERE iEventGroupID = ".$eventData["ConferenceID"]);
    
-	if (mysql_num_rows($eventQuery) != 1)
+	if (mysqli_num_rows($eventQuery) != 1)
     {
        // return array("status"=>"Fail", "message"=>"Could not find event!"); 
     }
     else
     {
-        $event = mysql_fetch_object($eventQuery);
+        $event = mysqli_fetch_object($eventQuery);
     }
    
-    $check_exist1 = mysql_query("SELECT * FROM FloorLevel WHERE iEventID = ".$eventData["ConferenceID"]);
+    $check_exist1 = mysqli_query($connection,"SELECT * FROM FloorLevel WHERE iEventID = ".$eventData["ConferenceID"]);
 
-    if(mysql_num_rows($check_exist1) == 0)
+    if(mysqli_num_rows($check_exist1) == 0)
     {
         //echo mysql_num_rows($check_exist1); 
 		for($i = 0;$i<sizeof($eventData['Floorplans']);$i++ )
@@ -47,13 +47,13 @@ function Content_Upload (){
 			 $mapQuery = "Insert into FloorLevel (iEventID, iMapID, vLevelName, dLastUpdated) Select * from (Select ".$eventData["ConferenceID"].",".$eventData["Floorplans"][$i].",'".$event->name." Map ".($i+1)."', NOW()) AS TMP WHERE NOT EXISTS (SELECT * FROM FloorLevel  WHERE (iEventID= ".$eventData["ConferenceID"]." and iMapID = ".$eventData["Floorplans"][$i].") or vLevelName = '".$event->name." Map ".($i+1)."') LIMIT 1; ";
            // echo $mapQuery;
             
-			mysql_query($mapQuery);
+			mysqli_query($connection,$mapQuery);
 		}
           $floorPlanArray['new'] = array();
             $floorPlanArray['processed'] = array();
-            $floorplanQuery=mysql_query("Select ID, iMapID, vLevelName from FloorLevel where iEventID =".$eventData['ConferenceID']." order by ID");
+            $floorplanQuery=mysqli_query($connection,"Select ID, iMapID, vLevelName from FloorLevel where iEventID =".$eventData['ConferenceID']." order by ID");
        
-            while ($fp = mysql_fetch_object($floorplanQuery))
+            while ($fp = mysqli_fetch_object($floorplanQuery))
                array_push($floorPlanArray['new'],array("FloorID"=>$fp->ID, "MapID"=>$fp->iMapID, "LevelName"=>$fp->vLevelName));
          return getTopMap($floorPlanArray['new'],$floorPlanArray['processed'], array("status"=>"Success", "EventID"=>$eventData["ConferenceID"], "FloorLevel"=>$floorPlanArray));
     }
@@ -78,7 +78,7 @@ function getTopMap (&$arrayIn_from, &$arrayIn_to, $addIn)
 {
     $getMapID = array_shift($arrayIn_from);
     array_push($arrayIn_to,$getMapID);
-    $MapObject  = mysql_fetch_array(mysql_query("Select * from images where ID =".$getMapID['MapID']));
+    $MapObject  = mysqli_fetch_array(mysqli_query($connection,"Select * from images where ID =".$getMapID['MapID']));
     return array_merge($addIn, array("currentFloorID"=>$getMapID['FloorID'], "currentFloorMap"=>$MapObject ));
 }
 
@@ -90,8 +90,8 @@ function Create_Rooms ()
     $eventData = $_POST;
     //var_dump ($_POST);
     $floorInfoArray = json_decode(stripslashes($_POST['FloorLevel']),true);
-    $getMapID=mysql_fetch_array(mysql_query("Select iMapID from FloorLevel where ID =".$eventData['currentFloorID']));
-    $MapObject  = mysql_fetch_array(mysql_query("Select * from images where ID =".$getMapID['iMapID']));
+    $getMapID=mysqli_fetch_array(mysqli_query($connection,"Select iMapID from FloorLevel where ID =".$eventData['currentFloorID']));
+    $MapObject  = mysqli_fetch_array(mysqli_query($connection,"Select * from images where ID =".$getMapID['iMapID']));
     return array("status"=>"Success", "EventID"=>$eventData["ConferenceID"], "FloorLevel"=>$floorInfoArray, "currentFloorID"=>$eventData['currentFloorID'], "currentFloorMap"=>$MapObject);
     /*
     $getMapID = array_shift($_POST['FloorLevel']);
@@ -113,12 +113,12 @@ function Final_Review()
 //fill up options for event 
 $EventGroup = array();
 $FloorPlans = array();
-$query = mysql_query("SELECT DISTINCT iEventGroupID, vGroupName FROM Sessions WHERE dSessionBegin >= DATE_FORMAT( CURDATE( ) ,  '%Y-1-1' )");
-while ($row = mysql_fetch_object($query))
+$query = mysqli_query($connection,"SELECT DISTINCT iEventGroupID, vGroupName FROM Sessions WHERE dSessionBegin >= DATE_FORMAT( CURDATE( ) ,  '%Y-1-1' )");
+while ($row = mysqli_fetch_object($query))
    $EventGroup[$row->iEventGroupID]=$row->vGroupName;
 
-$query = mysql_query("SELECT ID, ImageName, fileName FROM images");
-while ($row = mysql_fetch_object($query))
+$query = mysqli_query($connection, "SELECT ID, ImageName, fileName FROM images");
+while ($row = mysqli_fetch_object($query))
    $FloorPlans[$row->ID]=$row->ImageName;
 
 
