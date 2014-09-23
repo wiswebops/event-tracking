27e33d0514e93ae2c4171695b6a518dc84932d0d
yi guo <yi.guo@wispubs.com>
2014-09-23 15:38:13 -0400

    push 5

--- ConnectionTest.php
+++ ConnectionTest.php
@@ -1,26 +1,8 @@
 <?php
-
-
-
-$connection = mysql_connect('54.227.215.252', 'b2b2bd32573705', '63a5494f');
-mysql_select_db( 'heroku_f795c861b3c79e8',$connection);
-
-$query = mysql_query( "SELECT ID, ImageName, fileName FROM images", $connection);
-echo  mysql_num_rows($query); 
-/*
- $url=parse_url(getenv("us-cdbr-iron-east-01.clear.net"));
-
- $server = $url["54.227.215.252"];
-$username = $url["b2b2bd32573705"];
-$password = $url["63a5494f"];
-$db = substr($url["heroku_f795c861b3c79e8"],1);
-
-    $connection=mysqli_connect('54.227.215.252', 'b2b2bd32573705', '63a5494f');
-
-
-    mysqli_select_db($connection,'heroku_f795c861b3c79e8');
-$query = mysqli_query($connection, "SELECT ID, ImageName, fileName FROM images");
+$connection=mysqli_connect('54.227.215.252', 'b2b2bd32573705', '63a5494f');
+mysqli_select_db('heroku_f795c861b3c79e8');
+$query = mysqli_query("SELECT ID, ImageName, fileName FROM images");
 echo  mysqli_num_rows($query);
-*/
+
    
 ?>
