4b781659d960bb6eabb4f1199fd5a46d6549976f
yi guo <yi.guo@wispubs.com>
2014-09-23 14:39:47 -0400

    push 2

--- ConnectionTest.php
+++ ConnectionTest.php
@@ -1,7 +1,7 @@
 <?php
 
-
-define('DB_HOST','localhost');
+echo 'hahaha';
+define('DB_HOST','54.227.215.252');
 define('DB_USER','b2b2bd32573705');
 define('DB_PASS','63a5494f');
 define('DB_NAME','heroku_f795c861b3c79e8');
