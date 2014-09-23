9bd2ba612a09f808d12ff97134fb56fd63aa7ed8
YIGUO-WIS <yi.guo@wispubs.com>
2014-09-23 15:57:21 -0400

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,26 +1,8 @@
 <?php
-/*
-    $url=parse_url(getenv("us-cdbr-iron-east-01.clear.net"));
-
-    $server = $url["54.227.215.252"];
-    $username = $url["b2b2bd32573705"];
-    $password = $url["63a5494f"];
-    $db = substr($url["heroku_f795c861b3c79e8"],1);
-
-    mysqli_connect($server, $username, $password);
-
-
-    mysqli_select_db($db);
-*/
-
-
 define('DB_HOST','localhost');
 define('DB_USER','b2b2bd32573705');
 define('DB_PASS','63a5494f');
 define('DB_NAME','heroku_f795c861b3c79e8');
-
-$connection = mysql_connect(DB_HOST,DB_USER,DB_PASS) or die(mysql_error());
-mysql_select_db(DB_NAME) or die(mysql_error());
-
-
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS) or die(mysql_error());
+mysqli_select_db($connection, DB_NAME) or die(mysql_error());
 ?>
