e7c58a2f8913e9fba3425e5de31260915725255a
yi guo <yi.guo@wispubs.com>
2014-09-23 15:12:59 -0400

    push 4

--- ConnectionTest.php
+++ ConnectionTest.php
@@ -1,15 +1,12 @@
 <?php
 
 
-define('DB_HOST','54.227.215.252');
-define('DB_USER','b2b2bd32573705');
-define('DB_PASS','63a5494f');
-define('DB_NAME','heroku_f795c861b3c79e8');
 
 $connection = mysql_connect('54.227.215.252', 'b2b2bd32573705', '63a5494f');
-mysql_select_db('heroku_f795c861b3c79e8');
-$query = mysql_query( "SELECT ID, ImageName, fileName FROM images");
-echo  mysql_num_rows($query);
+mysql_select_db( 'heroku_f795c861b3c79e8',$connection);
+
+$query = mysql_query( "SELECT ID, ImageName, fileName FROM images", $connection);
+echo  mysql_num_rows($query); 
 /*
  $url=parse_url(getenv("us-cdbr-iron-east-01.clear.net"));
 
