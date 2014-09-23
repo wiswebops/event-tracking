97a2bd236b89f4c6134d8934bb2c716d783f5d35
yi guo <yi.guo@wispubs.com>
2014-09-23 14:11:19 -0400

    Added ConnectionTest file and modified connection.php

---
+++ ConnectionTest.php
@@ -0,0 +1,13 @@
+<?php
+
+
+define('DB_HOST','localhost');
+define('DB_USER','b2b2bd32573705');
+define('DB_PASS','63a5494f');
+define('DB_NAME','heroku_f795c861b3c79e8');
+
+$connection = mysql_connect(DB_HOST,DB_USER,DB_PASS) or die(mysql_error());
+mysql_select_db(DB_NAME) or die(mysql_error());
+
+
+?>
