c5b1f8b4d6b963e1c3135389af25e7bd48d9c1b2
YIGUO-WIS <yi.guo@wispubs.com>
2014-09-23 16:08:02 -0400

    Update addFloorplan.php

--- addFloorplan.php
+++ addFloorplan.php
@@ -56,8 +56,8 @@ $('button').live('click', function(){
 					 <select name="ConfID" type="text" class="form-control" required autofocus>
                     <?php
                           echo '<option class="form-control" value = "-1" disabled">Select a Conference</option>';                          
-                          $query = mysql_query("SELECT DISTINCT iEventGroupID, vGroupName FROM Sessions WHERE dSessionBegin >= DATE_FORMAT( CURDATE( ) ,  '%Y-1-1')");
-		                  while ($row = mysql_fetch_object($query))
+                          $query = mysqli_query($connection, "SELECT DISTINCT iEventGroupID, vGroupName FROM Sessions WHERE dSessionBegin >= DATE_FORMAT( CURDATE( ) ,  '%Y-1-1')");
+		                  while ($row = mysqli_fetch_object($query))
 			              echo '<option value="'.$row->iEventGroupID.'" >'.$row->vGroupName.'</option>';
                     ?>
                     </select>
