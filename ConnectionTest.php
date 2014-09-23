6430fbc8becf5caa2ebefb4144497779269d22ad
YIGUO-WIS <yi.guo@wispubs.com>
2014-09-23 14:32:40 -0400

    Update ConnectionTest.php

--- ConnectionTest.php
+++ ConnectionTest.php
@@ -9,5 +9,7 @@ define('DB_NAME','heroku_f795c861b3c79e8');
 $connection = mysql_connect(DB_HOST,DB_USER,DB_PASS) or die(mysql_error());
 mysql_select_db(DB_NAME) or die(mysql_error());
 
-
+$query = mysql_query("SELECT ID, ImageName, fileName FROM images",$connection );
+echo  mysql_num_rows($query);
+   
 ?>
