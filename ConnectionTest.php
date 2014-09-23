b01bd048497d6a5e0057c05d057951c044f74f4c
yi guo <yi.guo@wispubs.com>
2014-09-23 15:41:15 -0400

    push 3

--- ConnectionTest.php
+++ ConnectionTest.php
@@ -1,7 +1,7 @@
 <?php
 $connection=mysqli_connect('54.227.215.252', 'b2b2bd32573705', '63a5494f');
-mysqli_select_db('heroku_f795c861b3c79e8');
-$query = mysqli_query("SELECT ID, ImageName, fileName FROM images");
+mysqli_select_db($connection,'heroku_f795c861b3c79e8');
+$query = mysqli_query($connection,"SELECT ID, ImageName, fileName FROM images");
 echo  mysqli_num_rows($query);
 
    
