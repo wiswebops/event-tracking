16ec7c54539bdb9b37e9073357d404426c6c09fc
YIGUO-WIS <yi.guo@wispubs.com>
2014-09-23 15:56:19 -0400

    Update ConnectionTest.php

--- ConnectionTest.php
+++ ConnectionTest.php
@@ -1,8 +1,4 @@
 <?php
 $connection=mysqli_connect('54.227.215.252', 'b2b2bd32573705', '63a5494f');
 mysqli_select_db($connection,'heroku_f795c861b3c79e8');
-$query = mysqli_query($connection,"SELECT ID, ImageName, fileName FROM images");
-echo  mysqli_num_rows($query);
-
-   
 ?>
